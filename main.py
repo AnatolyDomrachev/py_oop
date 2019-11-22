737d1021f82f42ffe4988ac01e77f7fed5af2ef4
Ваше Имя <you@example.com>
2019-11-22 09:47:05 +0300

    Почти готово

--- main.py
+++ main.py
@@ -1,84 +1,13 @@
-class HouseScheme:
-    def __init__(self, room_num=-1, S=-1, su=-1):
-        self.room_num = room_num
-        self.S = S
-        self.su = su
+import prog
 
-        if not self.room_num in range(20) or not self.S in range(20) or not self.su in (True, False):
-            raise ValueError('Invalid value')
-
-class CountryHouse(HouseScheme):
-    def __init__(self, room_num=-1, S=-1, su=-1, et=-1, Su=-1 ):
-        super().__init__(room_num,S,su)
-        self.et=et
-        self.Su=Su
-
-        if not self.et in range(1,30) or not self.Su in range(1000000):
-            raise ValueError('Invalid value')
-
-    def __str__(self):
-        string = 'Country House: Количество жилых комнат '+str(self.room_num)+', Жилая площадь '+str(self.S)+', Совмещенный санузел '+str(self.su)+', Количество этажей '+str(self.et)+', Площадь участка '+str(self.Su)
-        return string
-
-    def __eq__(self, other):
-        if self.S == other.S and self.Su == other.Su and abs(self.et - other.et) <= 1 :
-            return True
-        else:
-            return False
-
-class Apartment(HouseScheme):
-    def __init__(self, room_num=-1, S=-1, su=-1, et=-1, okna=-1 ):
-        super().__init__(room_num,S,su)
-        self.et=et
-        self.okna=okna
-        if not self.et in range(1,16) and not self.okna in ('N','S','W','E'):
-            raise ValueError('Invalid value')
-    def __str__(self):
-        string = 'Apartment: Количество жилых комнат '+str(self.room_num)+', Жилая площадь '+str(self.S)+', Совмещенный санузел '+str(self.su)+', Этаж '+str(self.et)+', Окна выходят на '+self.okna
-        return string
-
-class CountryHouseList(list):
-    def __init__(self, name):
-        super().__init__()
-        self.name = name
-
-    def append(self, p_object):
-        if type(p_object) == CountryHouse:
-            super().append(p_object)
-        else:
-            error = 'Invalid type '+str(type(p_object))
-            raise TypeError(error)
-
-    def total_square(self):
-        ts = 0
-        for ch in self:
-            ts += ch.S
-        return ts
-
-class ApartmentList(list):
-    def __init__(self, name):
-        super().__init__()
-        self.name = name
-
-    def extend(self, iterable):
-        for element in iterable:
-            if type(element) == Apartment:
-                self.append(element)
-    def floor_view(self, floors, directions):
-        view = list(filter(lambda a: a.et in floors and a.okna in directions, self))
-        lv = list(view)
-        for el in lv:
-            print(el.okna+": "+str(el.et))
-
-
-a=HouseScheme(1,1,True)
-b=CountryHouse(1,1,1,1,1)
-c1=Apartment(1,1,1,1,'N')
-c2=Apartment(2,2,1,2,'S')
-c3=Apartment(3,3,1,3,'W')
-c4=Apartment(4,4,1,1,'N')
-chl=CountryHouseList("chl_name")
-al = ApartmentList("al_name")
+a = prog.HouseScheme(1,1,True)
+b = prog.CountryHouse(1,1,1,1,1)
+c1 = prog.Apartment(1,1,1,1,'N')
+c2 = prog.Apartment(2,2,1,2,'S')
+c3 = prog.Apartment(3,3,1,3,'W')
+c4 = prog.Apartment(4,4,1,1,'N')
+chl = prog.CountryHouseList("chl_name")
+al  = prog. ApartmentList("al_name")
 l1=[c1,c2]
 l2=[c3,c4]
 al.extend(l1)
