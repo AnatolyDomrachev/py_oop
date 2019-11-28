57a21a829a69c38b8a245144790847082d3b4d1c
AnatolyDomrachev <you@example.com>
2019-11-28 07:39:06 +0300

    anast

--- prog.py
+++ prog.py
@@ -1,40 +1,44 @@
 class HouseScheme:
-    def __init__(self, room_num, S, su):
-        self.room_num = room_num
-        self.S = S
-        self.su = su
+    def __init__(self, rooms, square, suz):
+        self.rooms = rooms
+        self.square = square
+        self.suz = suz
 
-        if not isinstance(self.room_num, int) or not self.S > 0  or not isinstance(self.su, bool):
+        if not isinstance(self.rooms, int):
+            raise ValueError('Invalid value')
+        if not isinstance(self.rooms, int):
+            raise ValueError('Invalid value')
+        if not isinstance(self.rooms, int):
             raise ValueError('Invalid value')
 
 class CountryHouse(HouseScheme):
-    def __init__(self, room_num, S, su, et, Su ):
-        super().__init__(room_num,S,su)
-        self.et=et
-        self.Su=Su
+    def __init__(self, rooms, square, suz, floor, land ):
+        super().__init__(rooms,square,suz)
+        self.floor=floor
+        self.land=land
 
-        if not self.et in range(1,30) or not self.Su in range(1000000):
+        if not self.floor in range(1,30) or not self.land in range(1000000):
             raise ValueError('Invalid value')
 
     def __str__(self):
-        string = 'Country House: Количество жилых комнат '+str(self.room_num)+', Жилая площадь '+str(self.S)+', Совмещенный санузел '+str(self.su)+', Количество этажей '+str(self.et)+', Площадь участка '+str(self.Su)+'.'
+        string = 'Country House: Количество жилых комнат '+str(self.rooms)+', Жилая площадь '+str(self.square)+', Совмещенный санузел '+str(self.suz)+', Количество этажей '+str(self.floor)+', Площадь участка '+str(self.land)+'.'
         return string
 
     def __eq__(self, other):
-        if self.S == other.S and self.Su == other.Su and abs(self.et - other.et) <= 1 :
+        if self.square == other.square and self.land == other.land and abs(self.floor - other.floor) <= 1 :
             return True
         else:
             return False
 
 class Apartment(HouseScheme):
-    def __init__(self, room_num, S, su, et, okna ):
-        super().__init__(room_num,S,su)
-        self.et=et
-        self.okna=okna
-        if not self.et in range(1,16) or not self.okna in ('N','S','W','E'):
+    def __init__(self, rooms, square, suz, floor, windows ):
+        super().__init__(rooms,square,suz)
+        self.floor=floor
+        self.windows=windows
+        if not self.floor in range(1,16) or not self.windows in ('N','S','W','E'):
             raise ValueError('Invalid value')
     def __str__(self):
-        string = 'Apartment: Количество жилых комнат '+str(self.room_num)+', Жилая площадь '+str(self.S)+', Совмещенный санузел '+str(self.su)+', Этаж '+str(self.et)+', Окна выходят на '+self.okna+'.'
+        string = 'Apartment: Количество жилых комнат '+str(self.rooms)+', Жилая площадь '+str(self.square)+', Совмещенный санузел '+str(self.suz)+', Этаж '+str(self.floor)+', Окна выходят на '+self.windows+'.'
         return string
 
 class CountryHouseList(list):
@@ -52,7 +56,7 @@ class CountryHouseList(list):
     def total_square(self):
         ts = 0
         for ch in self:
-            ts += ch.S
+            ts += ch.square
         return ts
 
 class ApartmentList(list):
@@ -65,8 +69,8 @@ class ApartmentList(list):
             if type(element) == Apartment:
                 self.append(element)
     def floor_view(self, floors, directions):
-        view = list(filter(lambda a: a.et in range(floors[0], floors[1]+1) and a.okna in directions, self))
+        view = list(filter(lambda a: a.floor in range(floors[0], floors[1]+1) and a.windows in directions, self))
         lv = list(view)
         for el in lv:
-            print(el.okna+": "+str(el.et))
+            print(el.windows+": "+str(el.floor))
 
